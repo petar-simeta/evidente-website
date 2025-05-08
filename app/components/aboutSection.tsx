8771f7596234a8b4c215d455b1374fd112a71649
Petar Šimeta <petar.simeta@perpetuum.hr>
2025-05-08 17:45:42 +0200

    fixed width problems in about section

--- app/components/aboutSection.tsx
+++ app/components/aboutSection.tsx
@@ -37,65 +37,58 @@ export default function AboutSection() {
   return (
     <section id='about' ref={sectionRef} className={styles.aboutSection}>
       <div className={styles.aboutWrapper}>
-        <div className='container'>
-          <div className={styles.grid} style={{ top: `${gridTop}%` }}>
-            <div className={styles.left}>
-              <h2 ref={headingRef}>
-                <span>
-                  {'It starts with'.split('').map((char, index) => (
-                    <motion.span
-                      key={index}
-                      initial={{ opacity: 0 }}
-                      animate={isInView ? { opacity: 1 } : { opacity: 0 }}
-                      transition={{ delay: 0.5 + index * 0.04 }}
-                      style={{
-                        display: char === ' ' ? 'inline' : 'inline-block',
-                      }}
-                    >
-                      {char}
-                    </motion.span>
-                  ))}
-                </span>
-                <span>
-                  {'emotion ✨'.split('').map((char, index) => (
-                    <motion.span
-                      key={index}
-                      initial={{ opacity: 0 }}
-                      animate={isInView ? { opacity: 1 } : { opacity: 0 }}
-                      transition={{ delay: 1.2 + index * 0.04 }}
-                      style={{
-                        display: char === ' ' ? 'inline' : 'inline-block',
-                      }}
-                    >
-                      {char}
-                    </motion.span>
-                  ))}
-                </span>
-              </h2>
-            </div>
-            <div className={styles.right}>
-              <motion.p
-                initial={{ opacity: 0, y: -20 }}
-                animate={
-                  isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }
-                }
-                transition={{ delay: 1.8, duration: 0.5 }}
-              >
-                And ends with eye-catching design, authentic stories, and
-                meaningful experiences.
-              </motion.p>
-              <motion.p
-                initial={{ opacity: 0, y: -20 }}
-                animate={
-                  isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }
-                }
-                transition={{ delay: 2.3, duration: 0.5 }}
-              >
-                We are a software agency in Croatia that cares about you and
-                your brand, no matter the size or what industry your business is
-                in.
-              </motion.p>
-            </div>
+        <div className={styles.grid} style={{ top: `${gridTop}%` }}>
+          <div className={styles.left}>
+            <h2 ref={headingRef}>
+              <span>
+                {'It starts with'.split('').map((char, index) => (
+                  <motion.span
+                    key={index}
+                    initial={{ opacity: 0 }}
+                    animate={isInView ? { opacity: 1 } : { opacity: 0 }}
+                    transition={{ delay: 0.5 + index * 0.04 }}
+                    style={{
+                      display: char === ' ' ? 'inline' : 'inline-block',
+                    }}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+              </span>
+              <span>
+                {'emotion ✨'.split('').map((char, index) => (
+                  <motion.span
+                    key={index}
+                    initial={{ opacity: 0 }}
+                    animate={isInView ? { opacity: 1 } : { opacity: 0 }}
+                    transition={{ delay: 1.2 + index * 0.04 }}
+                    style={{
+                      display: char === ' ' ? 'inline' : 'inline-block',
+                    }}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+              </span>
+            </h2>
+          </div>
+          <div className={styles.right}>
+            <motion.p
+              initial={{ opacity: 0, y: -20 }}
+              animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }}
+              transition={{ delay: 1.8, duration: 0.5 }}
+            >
+              And ends with eye-catching design, authentic stories, and
+              meaningful experiences.
+            </motion.p>
+            <motion.p
+              initial={{ opacity: 0, y: -20 }}
+              animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }}
+              transition={{ delay: 2.3, duration: 0.5 }}
+            >
+              We are a software agency in Croatia that cares about you and your
+              brand, no matter the size or what industry your business is in.
+            </motion.p>
           </div>
         </div>
       </div>
